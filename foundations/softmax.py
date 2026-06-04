2523a0bb4aea44e9312f8b50b05857fc45547863
Rananjay Singh Chauhan <rananjaychauhan93@gmail.com>
2026-06-04 21:55:52 +0530

    Update Softmax function (Math Foundations 3/6)

--- foundations/softmax.py
+++ foundations/softmax.py
@@ -1,23 +1,18 @@
 import numpy as np
 from numpy.typing import NDArray
-import math
+import math as muth
 
 class Solution:
-    
-    def sigmoid(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
-        # z is a 1D NumPy array
-        # Formula: 1 / (1 + e^(-z))
-        # return np.round(your_answer, 5)
-        anslist = []
-        for i in z:
-            answer = 1/(1 + math.e**(-i))
-            anslist.append(np.round(answer, 5))
-        return np.array(anslist)
 
-    def relu(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
-        # z is a 1D NumPy array
-        # Formula: max(0, z) element-wise
-        anslist = []
+    def softmax(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        # z is a 1D NumPy array of logits
+        # Hint: subtract max(z) for numerical stability before computing exp
+        # return np.round(your_answer, 4)
+        answers, denom = np.array([(np.round(muth.e**(x - max(z)), 4)) for x in z]), 0
         for i in z:
-            anslist.append(np.round(max(0.0, i), 2))
-        return np.array(anslist)
+            denom += (muth.e**(i - max(z)))
+        for i in range(len(answers)):
+            answers[i] = np.round(answers[i]/denom, 4)
+        
+        return answers
+            
